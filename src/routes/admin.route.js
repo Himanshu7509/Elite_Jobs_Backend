d3ec7983b45585be1f53e6d9741a269c8bf3d035
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-12-09 16:56:11 +0530

    45

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -15,12 +15,14 @@ const adminRouter = express.Router();
 
 // Protected routes - Admin only
 adminRouter.post('/jobs', authMiddleware, authorizeRole('admin'), createJobDirect);
-adminRouter.get('/applicants', authMiddleware, authorizeRole('admin'), getAllApplicants);
 adminRouter.delete('/users/:userId', authMiddleware, authorizeRole('admin'), deleteUserAccount);
 adminRouter.get('/jobs', authMiddleware, authorizeRole('admin'), getAllJobs);
 adminRouter.get('/applications', authMiddleware, authorizeRole('admin'), getAllApplications);
 adminRouter.get('/jobseekers/:id', authMiddleware, authorizeRole('admin'), getJobSeekerWithApplications);
 adminRouter.get('/jobseekers', authMiddleware, authorizeRole('admin'), getAllJobSeekers);
 adminRouter.get('/statistics/users', authMiddleware, authorizeRole('admin'), getUserStatistics); // Add the new route
+
+// Protected routes - Admin and EliteTeam
+adminRouter.get('/applicants', authMiddleware, authorizeRole('admin', 'eliteTeam'), getAllApplicants);
 
 export default adminRouter;
